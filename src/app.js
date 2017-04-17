2e751585ba925bbd60258da60b9cf4333ce0886d
Lewis <lewis.turner@Lewiss-Air.lan>
2017-04-17 20:58:40 +0100

    flattened chained promises

--- src/app.js
+++ src/app.js
@@ -6,74 +6,66 @@ var utils = require('./utils.js');
 // tests to see if DB has any data; runs dbInit functions if yes; runs executeOngoing if no
 sqlite.db.each("SELECT count(*) FROM Seasons", function(err, row) {
   if (err || row === undefined) {
-    dbInit.initDB().then(function(success) {
-      utils.getDateTime("initDB");
-      dbInit.getSeasons().then(function(success) {
-        utils.getDateTime("getSeasons");
-        dbInit.initData().then(function(success) {
-          utils.getDateTime("initData");
-          dbInit.setSeasons().then(function(success) {
-            utils.getDateTime("setSeasons");
-            executeOngoing();
-            // runOngoing();
-          }).catch(function(fail) {
-            console.log(fail);
-            console.log("1");
-          });
-        }).catch(function(fail) {
-          console.log(fail);
-          console.log("2");
-        });
-      }).catch(function(fail) {
-        console.log(fail);
-        console.log("3");
-      });
-    }).catch(function(fail) {
+    utils.getDateTime("Started DBInit")
+    .then(function() {
+      utils.getDateTime("Started initDB")
+      return dbInit.initDB();
+    })
+    .then(function() {
+      utils.getDateTime("Started getSeasons")
+      return dbInit.getSeasons();
+    })
+    .then(function() {
+      utils.getDateTime("Started initData")
+      return dbInit.initData();
+    })
+    .then(function() {
+      utils.getDateTime("Started setSeasons")
+      return dbInit.setSeasons();
+    })
+    .then(function() {
+      executeOngoing();
+    })
+    .catch(function(fail) {
       console.log(fail);
-      console.log("4");
     });
   } else {
     executeOngoing();
-    // runOngoing();
+    runOngoing();
   }
 });
 
 function executeOngoing() {
-  ongoing.getRaces().then(function(success) {
-    utils.getDateTime("getRaces");
-    ongoing.processSeasonRaces().then(function(success) {
-      utils.getDateTime("processRaces");
-      ongoing.getTeams().then(function(success) {
-        utils.getDateTime("getTeams");
-        ongoing.processTeams().then(function(success) {
-          utils.getDateTime("processTeams");
-          ongoing.getDrivers().then(function(success) {
-            utils.getDateTime("getDrivers");
-            ongoing.processDrivers().then(function(success) {
-              utils.getDateTime("processDrivers");
-              ongoing.processDriverTeams().then(function(success) {
-                utils.getDateTime("processDriverTeams");
-              }).catch(function(fail) {
-                console.log(fail);
-              })
-            }).catch(
-              function(fail) {
-                console.log(fail);
-              }
-            );
-          }).catch(function(fail) {
-            console.log(fail);
-          });
-        }).catch(function(fail) {
-          console.log(fail);
-        });
-      }).catch(function(fail) {
-        console.log(fail);
-      })
-    }).catch(function(err) {
-      console.log(err);
-    });
-  }).catch(function(fail) {
+  utils.getDateTime("Started executeOngoing")
+  .then(function() {
+    utils.getDateTime("Started getRaces")
+    return ongoing.getRaces();
+  })
+  .then(function() {
+    utils.getDateTime("Started processSeasonRaces")
+    return ongoing.processSeasonRaces();
+  })
+  .then(function() {
+    utils.getDateTime("Started getTeams")
+    return ongoing.getTeams();
+  })
+  .then(function() {
+    utils.getDateTime("Started processTeams")
+    return ongoing.processTeams();
+  })
+  .then(function() {
+    utils.getDateTime("Started getDrivers")
+    return ongoing.getDrivers()
+  })
+  .then(function() {
+    utils.getDateTime("Started processDrivers")
+    return ongoing.processDrivers()
+  })
+  .then(function() {
+    utils.getDateTime("Started processDriverTeams")
+    return ongoing.processDriverTeams()
+  })
+  .catch(function(fail) {
     console.log(fail);
   });
 }
--- src/utils.js
+++ src/utils.js
@@ -1,26 +1,29 @@
 var sqlite = require('./sqlite');
 
 module.exports.getDateTime = function(calledFrom) {
-  var date = new Date();
+  return new Promise(function (resolve, reject) {
+    var date = new Date();
 
-  var hour = date.getHours();
-  hour = (hour < 10 ? "0" : "") + hour;
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
 
-  var min  = date.getMinutes();
-  min = (min < 10 ? "0" : "") + min;
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
 
-  var sec  = date.getSeconds();
-  sec = (sec < 10 ? "0" : "") + sec;
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
 
-  var year = date.getFullYear();
+    var year = date.getFullYear();
 
-  var month = date.getMonth() + 1;
-  month = (month < 10 ? "0" : "") + month;
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
 
-  var day  = date.getDate();
-  day = (day < 10 ? "0" : "") + day;
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
 
-  console.log(calledFrom + " executed: " + year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec);
+    console.log(calledFrom + " executed: " + year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec);
+    return resolve("success");
+  });
 }
 
 module.exports.getDriverIDFromDriverRef = function(driverRef) {
